c5128e10305be4e974b828fbd313c90207dfa9a0
Kareem Kouddous <kareemknyc@gmail.com>
2015-07-29 18:06:52 -0400

    Bug Fix: Use attributes in event NOT current instance

--- spec/integration/create_spec.rb
+++ spec/integration/create_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Sorceror, 'create' do
   before do
+    $observer_model = nil
     $observer_fired_count = 0
   end
 
@@ -20,6 +21,7 @@ RSpec.describe Sorceror, 'create' do
       group :consistency
 
       observer :created, CreateModel => :created do |model|
+        $observer_model = model
         $observer_fired_count += 1
       end
     end
@@ -123,6 +125,20 @@ RSpec.describe Sorceror, 'create' do
       process!
 
       expect($observer_fired_count).to eq(1)
+    end
+  end
+
+  context 'when the underlying model changes while processing an event' do
+    let(:id) { BSON::ObjectId.new }
+
+    it 'does not use the pass the latest state of the model to the observer' do
+      CreateModel.new(id: id, field_1: 'field_1').create
+
+      process_operations!
+      CreateModel.collection.find(_id: id).update('$set' => { field_1: 'field_1_updated' })
+      process_events!
+
+      expect($observer_model.field_1).to eq('field_1')
     end
   end
 end
